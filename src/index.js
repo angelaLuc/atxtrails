7f8ab523fd3f779a0bee7e6fdb111ab43d95e7b4
Angela Lucignani <contactangelal@gmail.com>
2020-02-22 13:46:01 -0600

    update sw

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/js/bootstrap.bundle";
 import "./scss/app.scss";
 import App from "./core/app";
+import * as serviceWorker from './serviceWorker';
 
 let mountNode = document.getElementById("root");
 ReactDOM.render(<App />, mountNode);
+serviceWorker.unregister();
